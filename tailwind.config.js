efc76bb835016198468e61c3aca0ea20cde0af7c
Michael Caridad <caridadmichael@gmail.com>
2023-02-03 16:11:27 -0400

    create and style intro

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-brown': '#7A4B4B',
+        'light-brown' : '#BF722E',
+        'medium-brown' : '#916060'
+      },
+      fontFamily: {
+         'code' : ['Roboto-Mono','sans-serif'],
+         'inter': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
